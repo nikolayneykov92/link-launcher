fb7bc4b2edd25c61db39e7e3cd2de9ddb8b2cf29
Teodora Zaharieva <teodora@gtmhub.com>
2022-06-26 11:00:57 +0300

    initial client setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>Link Launcher</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
